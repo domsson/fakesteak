1335c7862b8c69c2f46a859be17c8f5a38aefd18
domsson <julien.d.dau@gmail.com>
2020-11-06 18:14:01 +0900

    Color and hiding the cursor

--- src/charrain.c
+++ src/charrain.c
@@ -1,9 +1,20 @@
-#include <stdio.h>      // fprintf(), stdout
+#include <stdio.h>      // fprintf(), stdout, setlinebuf()
 #include <stdlib.h>     // EXIT_SUCCESS, EXIT_FAILURE, rand()
 #include <unistd.h>     // sleep()
 #include <signal.h>     // sigaction(), struct sigaction
 #include <termios.h>    // struct winsize 
 #include <sys/ioctl.h>  // ioctl(), TIOCGWINSZ
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+#define ANSI_HIDE_CURSOR "\e[?25l"
+#define ANSI_SHOW_CURSOR "\e[?25h"
 
 static volatile int resize;
 static volatile int running;   // controls running of the main loop 
@@ -36,7 +47,8 @@ void get_size()
 char rand_ascii()
 {
 	int r = rand() % 126;
-	return r < 32 ? r + 32 : r;
+	//return r < 32 ? r + 32 : r;
+	return r < 32 ? 32 : r;
 
 }
 
@@ -52,9 +64,11 @@ int main(int argc, char **argv)
 
 	struct winsize ws = { 0 };
 	ioctl(0, TIOCGWINSZ, &ws);
-	
-	char *rain = malloc(ws.ws_col);
-	if (rain == NULL) return EXIT_FAILURE;
+
+	setlinebuf(stdout);
+
+	fprintf(stdout, ANSI_HIDE_CURSOR);
+	fprintf(stdout, ANSI_COLOR_GREEN);
 
 	running = 1;
 	while(running)
@@ -62,7 +76,6 @@ int main(int argc, char **argv)
 		if (resize)
 		{
 			ioctl(0, TIOCGWINSZ, &ws);
-			rain = realloc(rain, ws.ws_col);
 			resize = 0;
 			printf("\033[2J");             // clear screen
 			printf("\033[%dA", ws.ws_row); // move up ws.ws_row lines
@@ -70,13 +83,13 @@ int main(int argc, char **argv)
 
 		for (int i = 0; i < (ws.ws_col - 1); ++i)
 		{
-			rain[i] = rand_ascii();
+			fprintf(stdout, "%c", rand_ascii()); 
 		}
-		rain[ws.ws_col - 1] = '\0';
-
-		fprintf(stdout, "%s\n", rain);
+		fprintf(stdout, "\n");
 
 		sleep(1);
 	}
+	fprintf(stdout, ANSI_COLOR_RESET);
+	fprintf(stdout, ANSI_SHOW_CURSOR);
 	return EXIT_SUCCESS;
 }
