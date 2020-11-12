47c083e59926ef0513e7a5ee3e39bece82878ff7
domsson <julien.d.dau@gmail.com>
2020-11-12 17:24:50 +0900

    This is pretty neat now, actually

--- src/charrain.c
+++ src/charrain.c
@@ -1,7 +1,9 @@
 #include <stdio.h>      // fprintf(), stdout, setlinebuf()
 #include <stdlib.h>     // EXIT_SUCCESS, EXIT_FAILURE, rand()
 #include <stdint.h>     // uint8_t, uint16_t
+#include <math.h>       // ceil()
 #include <time.h>       // time(), nanosleep(), struct timespec
+#include <string.h>     // memmove()
 #include <signal.h>     // sigaction(), struct sigaction
 #include <termios.h>    // struct winsize 
 #include <sys/ioctl.h>  // ioctl(), TIOCGWINSZ
@@ -26,9 +28,12 @@
 #define DEBUG_TSIZE 3
 
 #define TSIZE_MIN 8
-#define TSIZE_MAX 252
+#define TSIZE_MAX 63
 
-#define GLITCH_RATIO 0.02
+//#define TSIZE_MIN 2
+//#define TSIZE_MAX 8
+
+#define GLITCH_RATIO 0.015
 #define DROP_RATIO   0.015
 
 static volatile int resized;   // window resize event received
@@ -68,6 +73,8 @@ typedef struct matrix
 	uint16_t *data;
 	int cols;
 	int rows;
+	size_t drop_count;
+	float  drop_ratio;
 }
 matrix_s;
 
@@ -80,7 +87,6 @@ on_signal(int sig)
 			resized = 1;
 			break;
 		case SIGINT:
-		case SIGKILL:
 		case SIGQUIT:
 		case SIGTERM:
 			running = 0;
@@ -276,7 +282,6 @@ mat_print(matrix_s *mat)
 					color_fg(color);
 					fprintf(stdout, "%c", val_get_ascii(value));
 					break;
-						
 			}
 		}
 		if (!last)
@@ -291,162 +296,197 @@ mat_print(matrix_s *mat)
 static void
 mat_debug(matrix_s *mat, int what)
 {
+	fprintf(stdout, "\x1b[0m");
+
 	uint16_t value = 0;
+	uint8_t  last  = 0;
+
 	for (int r = 0; r < mat->rows; ++r)
 	{
+		last = r == mat->rows - 1;
 		for (int c = 0; c < mat->cols; ++c)
 		{
 			value = mat_get_value(mat, r, c);
-			if (what == DEBUG_STATE)
+			switch (what)
 			{
-				fprintf(stdout, "%hhu", val_get_state(value));
-			}
-			else if (what == DEBUG_ASCII)
-			{
-				fprintf(stdout, "%c", val_get_ascii(value));
-			}
-			else if (what == DEBUG_TSIZE)
-			{
-				fprintf(stdout, "%hhu", val_get_tsize(value));
+				case DEBUG_STATE:
+					fprintf(stdout, "%hhu", val_get_state(value));
+					break;
+				case DEBUG_ASCII:
+					fprintf(stdout, "%c",   val_get_ascii(value));
+					break;
+				case DEBUG_TSIZE:
+					fprintf(stdout, "%hhu", val_get_tsize(value));
+					break;
 			}
 		}
-		fprintf(stdout, "\n");
-	}
-}
-
-static void
-mat_drop(matrix_s *mat, int max_tries)
-{
-	// TODO this could turn into an endless loop if there isn't any space
-	//      so we need to make sure the tail lengths are short enough in
-	//      relation to the terminal height to _always_ allow for a new
-	//      drop to be placed _somewhere_ at the top at least. Even then 
-	//      this isn't exactly an elegant approach... but oh well.
-
-	int maxlen = TSIZE_MAX >= mat->rows ? mat->rows - 1 : TSIZE_MAX;
-
-	while (max_tries--)
-	{
-		int c = rand_int(0, mat->cols - 1);
-		if (mat_get_state(mat, 0, c) == STATE_NONE && mat_get_state(mat, 1, c) == STATE_NONE)
+		if (!last)
 		{
-			mat_set_state(mat, 0, c, STATE_DROP);
-			mat_set_tsize(mat, 0, c, rand_int(TSIZE_MIN, maxlen));
-			break;
+			fprintf(stdout, "\n");
+		}
+	}
+	fflush(stdout);
+}
+
+/*
+ * Turn the specified cell into a DROP cell.
+ */
+static void
+mat_put_cell_drop(matrix_s *mat, int row, int col, int tsize)
+{
+	mat_set_state(mat, row, col, STATE_DROP);
+	mat_set_tsize(mat, row, col, tsize);
+}
+
+/*
+ * Turn the specified cell into a TAIL cell.
+ */
+static void
+mat_put_cell_tail(matrix_s *mat, int row, int col, int tsize, int tnext)
+{
+	float intensity = (float) tnext / (float) tsize; // 1 for end of trace, 0.x for beginning
+	int color = ceil(5 * intensity); // TODO hardcoded, bad
+	mat_set_state(mat, row, col, STATE_TAIL);
+//	mat_set_tsize(mat, row, col, tnext); // TODO color index instead!
+	mat_set_tsize(mat, row, col, color);
+}
+
+/*
+ * Add a DROP, including its TAIL cells, to the matrix, 
+ * starting from the specified position.
+ */
+static void
+mat_add_drop(matrix_s *mat, int row, int col, int tsize)
+{
+	for (int i = 0; i <= tsize; ++i, --row)
+	{
+		if (row < 0)          break;
+		if (row >= mat->rows) continue;
+
+		if (i == 0)
+		{
+			mat_put_cell_drop(mat, row, col, tsize);
+		}
+		else
+		{
+			mat_put_cell_tail(mat, row, col, tsize, i);
 		}
 	}
 }
 
+/*
+ * Make it rain by adding some DROPs to the matrix.
+ */
 static void
 mat_rain(matrix_s *mat, float ratio)
 {
-	int total = mat->cols * mat->rows;
-	int drops = (int) ((float) total * ratio);
+	int num = (int) (mat->cols * mat->rows) * ratio;
 
-	int maxlen = TSIZE_MAX >= mat->rows ? mat->rows - 1 : TSIZE_MAX;
+	int c = 0;
+	int r = 0;
 
-	for (int d = 0; d < drops; ++d)
+	for (int i = 0; i < num; ++i)
 	{
-		int r = rand_int(0, mat->rows - 1);
-		int c = rand_int(0, mat->cols - 1);
-		mat_set_state(mat, r, c, STATE_DROP);
-		mat_set_tsize(mat, r, c, rand_int(TSIZE_MIN, maxlen));
+		c = rand_int(0, mat->cols - 1);
+		r = rand_int(0, mat->rows - 1);
+		mat_add_drop(mat, r, c, rand_int(TSIZE_MIN, TSIZE_MAX));
 	}
+
 }
 
 /*
- * Works its way up from a drop, adding TAIL cells and coloring them.
- *
- * col  : the column to work on
- * row  : row to start in (should be the first tail cell above the drop)
- * tsize: tail length (should have been extracted from the drop)
+ * Move every cell down one row, potentially adding a new tail cell at the top.
+ * Returns 1 if a DROP 'fell off the bottom', otherwise 0.
  */
-static void
-col_trace(matrix_s *mat, int col, int row, int tsize)
+static int
+mat_mov_col(matrix_s *mat, int col)
 {
-	int top = row - tsize > 0 ? row - tsize : 0;
-	float intensity = 1;
-	int color = 0;
-	for (int i = 0; row >= top; --row, ++i)
-	{
-		if (row == top)
-		{
-			mat_set_state(mat, row, col, STATE_NONE);
-		}
-		else
-		{	
-			intensity = ((float)i / (float)tsize);
-			color = 5 * intensity; // TODO hardcoded, bad
-			mat_set_state(mat, row, col, STATE_TAIL);
-			mat_set_tsize(mat, row, col, color + 1);
-		}
-	}
-}
+	uint8_t tail_size = 0;
+	uint8_t tail_seen = 0;
 
-/*
- * Works its way up from the bottom of a column, setting the color of each 
- * TAIL cell to that of the TAIL cell above and deletes the top-most TAIL 
- * cell of that bottom-most continuous trace.
- */
-static void
-col_clean(matrix_s *mat, int col)
-{
-	int state = STATE_NONE;
+	uint16_t value = 0;
+	uint8_t  state = STATE_NONE;
+	uint8_t  tsize = 0;
+
+	// manually check the bottom-most cell: is it a DROP?
+	int dropped = mat_get_state(mat, mat->rows - 1, col) == STATE_DROP;
+
+	// iterate all cells in this column, moving each down one cell
 	for (int row = mat->rows - 1; row >= 0; --row)
 	{
-		state = mat_get_state(mat, row, col);
+		// get the current cell's meta data
+		value = mat_get_value(mat, row, col);
+		state = val_get_state(value);
+		tsize = val_get_tsize(value);
+
+		// nothing to do if this cell is neither DROP nor TAIL
 		if (state == STATE_NONE)
 		{
-			mat_set_state(mat, row+1, col, STATE_NONE);
-			break;
+			continue;
 		}
-		else
+
+		// move the cell one down
+		mat_set_state(mat, row+1, col, state);
+		mat_set_tsize(mat, row+1, col, tsize);
+
+		// null the current cell
+		mat_set_state(mat, row, col, STATE_NONE);
+		mat_set_tsize(mat, row, col, 0);
+
+		// keep track of the tail length of the last seen drop
+		if (state == STATE_DROP)
 		{
-			mat_set_tsize(mat, row+1, col, mat_get_tsize(mat, row, col));
+			// remember the tail size to draw for this drop
+			tail_size = tsize;
+			tail_seen = 0;
+		}
+		else if (state == STATE_TAIL && tail_size > 0)
+		{
+			// keep track of how many tail cells we've seen
+			++tail_seen;
 		}
 	}
+
+	// if the top-most cell wasn't empty, we might have to add a tail cell
+	if (state != STATE_NONE && tail_seen < tail_size)
+	{
+		mat_put_cell_tail(mat, 0, col, tail_size, tail_seen + 1); 
+	}
+
+	return dropped;
 }
 
-static void
+static void 
 mat_update(matrix_s *mat)
 {
-	uint8_t state = STATE_NONE;
-	uint8_t tsize = 0;
+	// add new drops at the top, trying to get to the desired drop count
 
-	// find the drops
-	for (int c = 0; c < mat->cols; ++c)
+	size_t drops_desired = (mat->cols * mat->rows) * mat->drop_ratio;
+	size_t drops_missing = drops_desired - mat->drop_count; 
+	size_t drops_to_add  = (size_t) ((float) drops_missing / (float) mat->rows);
+
+	int col = 0;
+	//uint8_t state_this  = STATE_NONE;
+	//uint8_t state_below = STATE_NONE;
+
+	for (size_t i = 0; i <= drops_to_add; ++i)
 	{
-		for (int r = mat->rows - 1; r >= 0; --r)
+		col = rand_int(0, mat->cols - 1);
+		//state_this  = mat_get_state(mat, 0, col);
+		//state_below = mat_get_state(mat, 1, col);
+		
+		// only place drop when there is free space
+		// if (state_this == STATE_NONE && state_below == STATE_NONE)
 		{
-			state = mat_get_state(mat, r, c);
-			tsize = mat_get_tsize(mat, r, c);
-
-			if (state == STATE_DROP)
-			{
-				if (r == mat->rows - 1) // bottom row)
-				{
-					mat_set_state(mat, r, c, STATE_TAIL);
-					mat_drop(mat, mat->cols);
-					col_trace(mat, c, r, tsize);
-					col_clean(mat, c);
-				}
-				else
-				{
-					mat_set_state(mat, r,   c, STATE_NONE);
-					mat_set_state(mat, r+1, c, STATE_DROP);
-					mat_set_tsize(mat, r+1, c, tsize);
-					col_trace(mat, c, r, tsize);
-				}
-			}
-			else if (state == STATE_TAIL)
-			{
-				if (r == mat->rows - 1) // bottom row
-				{
-					col_clean(mat, c);
-				}
-
-			}
+			mat_add_drop(mat, 0, col, rand_int(TSIZE_MIN, TSIZE_MAX));
+			++mat->drop_count;
 		}
+	}
+
+	// move each column down one cell, possibly dropping some drops
+	for (int col = 0; col < mat->cols; ++col)
+	{
+		mat->drop_count -= mat_mov_col(mat, col);
 	}
 }
 
@@ -468,16 +508,21 @@ mat_fill(matrix_s *mat, uint8_t state)
  * Returns -1 on error (out of memory), 0 on success.
  */
 static int
-mat_init(matrix_s *mat, int rows, int cols)
+mat_init(matrix_s *mat, int rows, int cols, float drop_ratio)
 {
-	mat->data = realloc(mat->data, sizeof(uint16_t) * rows * cols);
-	if (mat->data)
+	mat->data = realloc(mat->data, sizeof(mat->data) * rows * cols);
+	if (mat->data == NULL)
 	{
-		mat->rows = rows;
-		mat->cols = cols;
-		return 0;
+		return -1;
 	}
-	return -1;
+	
+	mat->rows = rows;
+	mat->cols = cols;
+
+	mat->drop_count = 0;
+	mat->drop_ratio = drop_ratio;
+	
+	return 0;
 }
 
 void
@@ -528,7 +573,6 @@ main(int argc, char **argv)
 	// set signal handlers for the usual susspects plus window resize
 	struct sigaction sa = { .sa_handler = &on_signal };
 	sigaction(SIGINT,   &sa, NULL);
-	sigaction(SIGKILL,  &sa, NULL);
 	sigaction(SIGQUIT,  &sa, NULL);
 	sigaction(SIGTERM,  &sa, NULL);
 	sigaction(SIGWINCH, &sa, NULL);
@@ -542,15 +586,16 @@ main(int argc, char **argv)
 
 	// this will determine the speed of the entire thing
 	struct timespec ts = { .tv_sec = 0, .tv_nsec = 100000000 };
+	//struct timespec ts = { .tv_sec = 1, .tv_nsec = 0 };
 
 	// set the buffering to fully buffered, we're adult and flush ourselves
 	setvbuf(stdout, NULL, _IOFBF, 0);
 
 	// initialize the matrix
 	matrix_s mat = { 0 }; 
-	mat_init(&mat, ws.ws_row, ws.ws_col);
+	mat_init(&mat, ws.ws_row, ws.ws_col, DROP_RATIO);
 	mat_fill(&mat, STATE_NONE);
-	mat_rain(&mat, DROP_RATIO);
+	//mat_rain(&mat, DROP_RATIO);
 
 	// prepare the terminal for our shenanigans
 	cli_setup();
@@ -563,9 +608,9 @@ main(int argc, char **argv)
 			// get the new terminal dimensions
 			ioctl(0, TIOCGWINSZ, &ws);
 			// reinitialize the matrix
-			mat_init(&mat, ws.ws_row, ws.ws_col);
+			mat_init(&mat, ws.ws_row, ws.ws_col, DROP_RATIO);
 			mat_fill(&mat, STATE_NONE);
-			mat_rain(&mat, DROP_RATIO);
+			//mat_rain(&mat, DROP_RATIO);
 			resized = 0;
 		}
 
